2272b43502702ee6c15a7799136280231a8b37fb
Gokul <gokulnath0827@gmail.com>
2022-10-04 20:09:02 +0530

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,54 @@
 import React from 'react'
 import headerImg from '../assets/img/header-2.png'
 import Typewriter from 'typewriter-effect'
+import RegisterBtn from '../components/register-button/RegisterBtn'
+import FlipCountdown from '@rumess/react-flip-countdown';
+
 const Header = () => {
     return (
-        <header className="container header">
-            <div className="header-left">
-                <h1 className='title'>
-                    Hi There! 
-                    <Typewriter
-                        onInit={((typewriter) => {
-                            typewriter.typeString("DRESTEIN 2k22").start()
-                        })}
-                    /> is here.
-                </h1>
-                <p className="desc">13th National level inter collegiate technical and management fest </p>
+        <>
+            <header className="container header">
+                <div className="header-left">
+                    <h1 className='title'>
+                        Hi There!
+                        <Typewriter
+                            onInit={((typewriter) => {
+                                typewriter
+                                    .typeString("DRESTEIN 2k22")
+                                    .pauseFor(2000)
+                                    .deleteAll()
+                                    .typeString("DREAM")
+                                    .pauseFor(1000)
+                                    .deleteAll()
+                                    .typeString("DESIGN")
+                                    .pauseFor(1000)
+                                    .deleteAll()
+                                    .typeString("COMPETE")
+                                    .pauseFor(1000)
+                                    .deleteAll()
+                                    .typeString("WIN.")
+                                    .pauseFor(1000)
+                                    .deleteAll()
+                                    .typeString("DRESTEIN 2k22")
+                                    .start()
+                            })}
+                        /> is here.
+                    </h1>
+                    <p className="desc">13th National level inter collegiate technical and management fest </p>
+                    <RegisterBtn />
+                </div>
+                <div className="header-right">
+                    <img src={headerImg} alt="" srcset="" />
+                </div>
+            </header>
+            <div className="container countdown">
+                <FlipCountdown
+                    hideYear
+                    hideMonth
+                    endAt={'2022-12-12 01:26:58'} // Date/Time
+                />
             </div>
-            <div className="header-right">
-                <img src={headerImg} alt="" srcset="" />
-            </div>
-        </header>
+        </>
     )
 }
 
---
+++ src/components/register-button/RegisterBtn.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './registerBtn.scss'
+import { ImRocket } from 'react-icons/im'
+const RegisterBtn = () => {
+    return (
+        <div className="btn">
+            Register <span><ImRocket /></span>
+        </div>
+    )
+}
+
+export default RegisterBtn
